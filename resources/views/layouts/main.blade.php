35be406c7b7a7563f8635651fd79ec6ec8f011a2
unknown <dueduedudu02@gmail.com>
2024-03-26 18:05:24 -0300

    adicionando a criação de eventos

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -44,7 +44,17 @@
                     </div>
               </nav>
         </header>
-        @yield('content')
+        <main>
+            <div class="container-fluid">
+            <div class="row">
+                @if(session('msg'))
+                    <p class="msg">{{ session('msg') }}</p>
+                @endif
+                @yield('content')
+            </div>
+            </div>
+            
+        </main>
         <footer>
             <p>Search Events &copy; 2024</p>
         </footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,12 +21,12 @@
     <div id="cards-container" class="row">
         @foreach($events as $event)
         <div class="card col-md-3">
-            <img src="img/event_placeholder.jpg" alt="{{$event->title}}" title="{{$event->title}}">
+            <img src="img/events/{{ $event->image }}" alt="{{$event->title}}" title="{{$event->title}}">
             <div class="card-body">
                 <p class="card-date">15/04/2004</p>
                 <h5 class="card-title">{{$event->title}}</h5>
                 <p class="card-participants">X participantes</p>
-                <a href="" class="btn btn-primary pink">Saber mais</a>
+                <a href="" class="btn btn-primary">Saber mais</a>
             </div>
         </div>
         @endforeach
